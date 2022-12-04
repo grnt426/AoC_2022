a68aced05e31e929f68416a3dfbb7cc75f6319b6
Grant Kurtz <th3guys2@gmail.com>
2022-12-04 16:08:19 -0500

    Find sum of top 3 elves, Day 1.2

--- src/Day1/calorie_counting.ex
+++ src/Day1/calorie_counting.ex
@@ -1,20 +1,30 @@
 defmodule CalorieCounting do
   @moduledoc false
 
-  def find_highest(input) do
-    IO.puts(input)
+  def sum_elf_food(input, top_count) do
     elves = String.split(input)
-    IO.puts("Each Elf: #{elves}")
     elves_food = Enum.map(elves, fn f -> String.split(f, ",") end)
-    IO.puts("Their Food in a list: #{elves_food}, items #{length(elves_food)}")
-    summed = Enum.map(elves_food, fn l -> Enum.map(l, fn f -> String.to_integer(f) end) |> Enum.sum() end)
-#    Enum.sort(summed) |> Enum.reverse() |> IO.inspect(charlists: :as_lists)
-    IO.puts("Maximum food on an elf #{Enum.max(summed)}")
+    Enum.map(elves_food, fn l -> Enum.map(l, fn f -> String.to_integer(f) end)
+      |> Enum.sum() end) |> Enum.sort() |> Enum.reverse()
+      |> Enum.slice(0, top_count) |> Enum.sum()
   end
+
 end
 
-IO.puts("Example Puzzle")
+IO.puts("Example Puzzle. Expect: 24,000")
+case File.read("input/Day1/example.txt") do
+  {:ok, body} -> IO.puts(CalorieCounting.sum_elf_food(body, 1))
+  {:error, reason} -> IO.puts(reason)
+end
+
+IO.puts("Input 1 Puzzle. Expect 69,912")
 case File.read("input/Day1/input1.txt") do
-  {:ok, body} -> IO.puts(CalorieCounting.find_highest(body))
+  {:ok, body} -> IO.puts(CalorieCounting.sum_elf_food(body, 1))
+  {:error, reason} -> IO.puts(reason)
+end
+
+IO.puts("Input 2 Puzzle. Expect 208,180")
+case File.read("input/Day1/input1.txt") do
+  {:ok, body} -> IO.puts(CalorieCounting.sum_elf_food(body, 3))
   {:error, reason} -> IO.puts(reason)
 end
