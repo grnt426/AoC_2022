90a79ebd954b580f54886399fe0891e0d9f47d52
Grant Kurtz <th3guys2@gmail.com>
2022-12-04 14:42:36 -0500

    Finding max.
    Currently bugged, dunno why.

--- src/Day1/calorie_counting.ex
+++ src/Day1/calorie_counting.ex
@@ -7,15 +7,14 @@ defmodule CalorieCounting do
     IO.puts("Each Elf: #{elves}")
     elves_food = Enum.map(elves, fn f -> String.split(f, ",") end)
     IO.puts("Their Food in a list: #{elves_food}, items #{length(elves_food)}")
-#    elf_food_total = Enum.map(elves_food, fn l -> Enum.map(l, fn f -> String.to_integer(f) end) end)
-    max = Enum.map(elves_food, fn l -> Enum.sum(Enum.map(l, fn f -> String.to_integer(f) end)) end) |> Enum.max()
-    IO.puts(max)
+    summed = Enum.map(elves_food, fn l -> Enum.map(l, fn f -> String.to_integer(f) end) |> Enum.sum() end)
+#    Enum.sort(summed) |> Enum.reverse() |> IO.inspect(charlists: :as_lists)
+    IO.puts("Maximum food on an elf #{Enum.max(summed)}")
   end
 end
 
 IO.puts("Example Puzzle")
-case File.read("input/Day1/example.txt") do
+case File.read("input/Day1/input1.txt") do
   {:ok, body} -> IO.puts(CalorieCounting.find_highest(body))
   {:error, reason} -> IO.puts(reason)
 end
-
