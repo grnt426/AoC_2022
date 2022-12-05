6ec4cf7d3d44ca26420bcc83ef59163f3f0fdfa6
Grant Kurtz <th3guys2@gmail.com>
2022-12-04 22:38:27 -0500

    Day 2 variant 2 working.

--- src/Day2/rps.ex
+++ src/Day2/rps.ex
@@ -13,6 +13,16 @@ defmodule Rps do
       fn g -> Enum.find_index(scoring, fn x -> x == String.trim(g) end) + 1 end
     ) |> Enum.sum()
   end
+
+  def play_to_strategy(input) do
+    play_guide = %{
+      "B X"=>"B X", "C Y"=>"C Z", "A Z"=>"A Y",
+      "A X"=>"A Z", "B Y"=>"B Y", "C Z"=>"C X",
+      "C X"=>"C Y", "A Y"=>"A X", "B Z"=>"B Z"
+    }
+    games = String.split(input, "\n")
+    Enum.map(games, fn g -> play_guide[String.trim(g)] end) |> Enum.join("\n") |> predict_score()
+  end
 end
 
 # A = Rock
@@ -32,5 +42,19 @@ end
 IO.puts("Input 1 Puzzle. Expect: 11,063")
 case File.read("input/Day2/input1.txt") do
   {:ok, body} -> IO.puts(Rps.predict_score(body))
+  {:error, reason} -> IO.puts(reason)
+end
+
+# A = Rock
+# B = Paper
+# C = Scissors
+
+# X = lose
+# Y = draw
+# Z = win
+
+IO.puts("Input 2 Puzzle. Expect: 10349")
+case File.read("input/Day2/input1.txt") do
+  {:ok, body} -> IO.puts(Rps.play_to_strategy(body))
   {:error, reason} -> IO.puts(reason)
 end
