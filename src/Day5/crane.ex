3c7a0c7860cf5e156058554883c1414f22681b97
Grant Kurtz <th3guys2@gmail.com>
2022-12-07 12:06:23 -0500

    Day 5 input 2 working

--- src/Day5/crane.ex
+++ src/Day5/crane.ex
@@ -1,14 +1,14 @@
 defmodule Crane do
   @moduledoc false
 
-  def find_top_crates(input) do
+  def find_top_crates(input, retain_order) do
     puzzle = String.split(input, "\n")
     stack_height = Enum.at(puzzle, 0) |> String.trim |> String.to_integer
     stacks = Enum.slice(puzzle, 1, stack_height - 1)
     numbered = Enum.at(puzzle, stack_height) |> String.split
     total_stacks = List.last(numbered) |> String.trim |> String.to_integer
     commands = Enum.slice(puzzle, stack_height + 2, length(puzzle) - stack_height - 1)
-    crate_stacks = build_stacks(stacks, total_stacks) |> process_moves(commands)
+    crate_stacks = build_stacks(stacks, total_stacks) |> process_moves(commands, retain_order)
     Enum.map(crate_stacks, fn s -> hd(s) end) |> Enum.to_list
   end
 
@@ -31,29 +31,28 @@ defmodule Crane do
     []
   end
   
-  defp process_moves(stacks, commands) when length(commands) > 0 do
+  defp process_moves(stacks, commands, retain_order) when length(commands) > 0 do
     [cur | rest] = commands
     [amt | [src | [dst]]] = Regex.scan(~r/(\d+)/, cur, capture: :first) |> List.flatten |> Enum.map(fn c -> String.to_integer(c) end)
     src = src - 1
     dst = dst - 1
     src_stack = Enum.at(stacks, src)
     dst_stack = Enum.at(stacks, dst)
-    IO.puts("SRC #{src_stack}")
-    IO.puts("DST #{dst_stack == nil}")
     
-    moved = Enum.slice(src_stack, 0, amt) |> Enum.reverse
+    moved = Enum.slice(src_stack, 0, amt)
+    moved = case retain_order do
+      true -> moved
+      false -> Enum.reverse(moved)
+    end
     src_stack = Enum.drop(src_stack, amt)
     dst_stack = Enum.concat(moved, dst_stack)
-
-    IO.puts("SRC #{src_stack}")
-    IO.puts("DST #{dst_stack}")
     
     stacks = List.replace_at(stacks, src, src_stack)
     stacks = List.replace_at(stacks, dst, dst_stack)
-    process_moves(stacks, rest)
+    process_moves(stacks, rest, retain_order)
   end
   
-  defp process_moves(stacks, commands) when length(commands) == 0 do
+  defp process_moves(stacks, commands, _) when length(commands) == 0 do
     stacks
   end
 end
@@ -63,12 +62,18 @@ end
 
 IO.puts("Example Puzzle. Expect: CMZ")
 case File.read("input/Day5/example.txt") do
-  {:ok, body} -> IO.puts("Result: #{Crane.find_top_crates(body)}")
+  {:ok, body} -> IO.puts("Result: #{Crane.find_top_crates(body, false)}")
   {:error, reason} -> IO.puts(reason)
 end
 
 IO.puts("Input 1 Puzzle. Expect: QNHWJVJZW")
 case File.read("input/Day5/input1.txt") do
-  {:ok, body} -> IO.puts("Result: #{Crane.find_top_crates(body)}")
+  {:ok, body} -> IO.puts("Result: #{Crane.find_top_crates(body, false)}")
+  {:error, reason} -> IO.puts(reason)
+end
+
+IO.puts("Input 2 Puzzle. Expect: BPCZJLFJW")
+case File.read("input/Day5/input1.txt") do
+  {:ok, body} -> IO.puts("Result: #{Crane.find_top_crates(body, true)}")
   {:error, reason} -> IO.puts(reason)
 end
