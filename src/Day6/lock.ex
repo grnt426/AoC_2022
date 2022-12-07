e71f827d20a81d0c1e29441fa703fbd38ef4ebdd
Grant Kurtz <th3guys2@gmail.com>
2022-12-07 12:55:03 -0500

    Day 6 input 2 working.

--- src/Day6/lock.ex
+++ src/Day6/lock.ex
@@ -1,21 +1,27 @@
 defmodule Lock do
   @moduledoc false
   
-  def find_lock(input) do
-    check = String.slice(input, 0, 4)
+  def find_lock(input, look_ahead) do
+    check = String.slice(input, 0, look_ahead)
     count = String.to_charlist(check) |> Enum.sort |> Enum.dedup |> List.to_string |> String.length
     case count do
-      4 -> 4
-      x when x < 4 -> 1 + find_lock(String.slice(input, 1, String.length(input)))
+      ^look_ahead -> look_ahead
+      x when x < look_ahead -> 1 + find_lock(String.slice(input, 1, String.length(input)), look_ahead)
     end
   end
 end
 
 IO.puts("Example Puzzle. Expect: 7")
-IO.puts("Result: #{Lock.find_lock("mjqjpqmgbljsphdztnvjfqwrcgsmlb")}")
+IO.puts("Result: #{Lock.find_lock("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 4)}")
 
 IO.puts("Input 1 Puzzle. Expect: 1,804")
 case File.read("input/Day6/input1.txt") do
-  {:ok, body} -> IO.puts("Result: #{Lock.find_lock(String.trim(body))}")
+  {:ok, body} -> IO.puts("Result: #{Lock.find_lock(String.trim(body), 4)}")
+  {:error, reason} -> IO.puts(reason)
+end
+
+IO.puts("Input 1 Puzzle. Expect: ")
+case File.read("input/Day6/input1.txt") do
+  {:ok, body} -> IO.puts("Result: #{Lock.find_lock(String.trim(body), 14)}")
   {:error, reason} -> IO.puts(reason)
 end
